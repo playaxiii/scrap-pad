dff8fc63d25a117caa9da13ca3b0fdb9390d30d6
Avinash Sharma <play.axiii@gmail.com>
2023-11-08 00:05:00 +0530

    Initial commit

---
+++ app/src/main/java/com/avi/scrap_pad/MainActivity.java
@@ -0,0 +1,14 @@
+package com.avi.scrap_pad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
